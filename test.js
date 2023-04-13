05a57bb745dc5b1d70e0cbfc15d7695a37d79427
Pastakun <130649332+Pastakun@users.noreply.github.com>
2023-04-13 19:14:41 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+const socket = new WebSocket('wss://192.168.0.40:10001');
+function send(){
+  socket.send(`${document.getElementsByName('name')[0].value}: ${document.getElementsByName('text')[0].value}`);
+}
+socket.addEventListener('message', (event) => {
+  let text = document.createElement('p');
+  text.prepend(event.data);
+  let textbox = document.getElementById('textbox');
+  textbox.prepend(text);
+});
