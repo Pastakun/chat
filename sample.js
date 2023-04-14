73858a5c5c34de7b92f23d62a0174a82df61a6fd
Pastakun <130649332+Pastakun@users.noreply.github.com>
2023-04-15 07:33:24 +0900

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,10 @@
+const socket = new WebSocket('wss://clouddata.turbowarp.org/');
+//クラウド変数更新
+function cloudsend(method,user,project_id,name,value) {
+	socket.send("".concat(JSON.stringify({"method":method,"user":user,"project_id":project_id,"name":name,"value":value}),"\n"));
+}
+
+// 接続が開いたときのイベント
+socket.addEventListener('open', function (event) {
+	cloudsend("handshake","user","625842821");
+});
